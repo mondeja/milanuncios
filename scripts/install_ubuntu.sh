5f56af78b46e500bf204ad2518c1f692bd67abc2
mondeja <mondejar1994@gmail.com>
2017-12-23 15:14:33 +0100

    Scripts folder deleted

--- scripts/install_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz
-tar -xvzf geckodriver-v0.19.1-linux64.tar.gz
-sudo mv geckodriver /usr/local/bin/geckodriver
-git clone https://github.com/mondeja/milanuncios.git
-cd milanuncios
-python3 setup.py install
