9a68315bc4df4d65f9677bb302bffb374fa215c2
mondeja <mondejar1994@gmail.com>
2017-12-24 16:32:20 +0100

    Travis testing

--- scripts/geckodriver/download.sh
+++ scripts/geckodriver/download.sh
@@ -3,10 +3,18 @@
 # Script to get neccesary version of geckodriver
 # across multiples OS. Need wget installed yet
 
-# URLs by OS and architecture
+
+if [ -z $1 ]  # travis is not first command?
+then
+  travis=0
+fi
+
+
+# =======   URLs by OS and architecture   =======
 Linux86_64=https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz
+# ===============================================
 
-
+# Get OS
 case "$OSTYPE" in
   linux*)   OS="linux" ;;
   darwin*)  OS="mac" ;;
@@ -17,21 +25,24 @@ case "$OSTYPE" in
 esac
 
 
-echo $OS
-
 # Linux environments
 if [ $OS == "linux" ]
 then
-  # Install wget
-  sudo apt-get install wget
-  # 64bit architecture
+
+  # Install wget?
+  if [ $travis -eq 0]  # If we are not in travis, try it
+  then
+    sudo apt-get install wget
+  fi
+
+  # 64bit architecture?
   if [ `uname -m` == "x86_64" ]
   then
     wget $Linux86_64
   fi
+
 fi
 
-ls
 
 
 
